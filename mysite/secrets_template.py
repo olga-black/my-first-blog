c5bb2435d171ab87e2852127ab39863bb9a7c96e
Olga <olha.v.diakonova@gmail.com>
2018-05-21 00:15:59 +0300

    Added a template file for storing confidential data

---
+++ mysite/secrets_template.py
@@ -0,0 +1,18 @@
+"""
+This is the confidential data for the file settings.py 
+of my website <foobar.com>
+"""
+
+
+def get_key():
+    """
+    My SECRET_KEY
+    To generate a new SECRET_KEY, you can use this script https://gist.github.com/mattseymour/9205591
+    """
+    return ''       # insert your SECRET_KEY here
+
+def get_hosts():
+    """
+    My ALLOWED_HOSTS
+    """
+    return []       # insert your host URL(s) here
