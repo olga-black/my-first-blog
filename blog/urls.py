aa678e42af069b2d782ae40db4adad7796432b75
Olga <olha.v.diakonova@gmail.com>
2018-07-15 22:08:37 +0300

    Added views to create/edit blog post

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # this is not sure - for adding pictures
