354ff641537887a0555055ee6c6956f8ebe26373
Olha <olha.v.diakonova@gmail.com>
2019-10-01 23:22:18 +0200

    Removed unused code

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from uuid import uuid4
-import os
-
-
-# def path_and_rename(instance, filename):
-#     upload_to = 'post_imgs'
-#     ext = filename.split('.')[-1]
-#     # get filename
-#     if instance.pk:
-#         filename = '{}.{}'.format(instance.pk, ext)
-#     else:
-#         # set filename as random string
-#         filename = '{}.{}'.format(uuid4().hex, ext)
-#     # return the whole path to the file
-#     return os.path.join(upload_to, filename)
 
 
 class Post(models.Model):
